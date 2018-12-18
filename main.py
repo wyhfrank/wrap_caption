24d754208668605a6fff17650c6ee74b679d46e1
Yuhao Wu <wyhfrank@gmail.com>
2018-12-18 13:11:52 +0900

    Fix bug: length of unicode chars are counted correctly due to encoding.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import io
 import in_place
 import argparse
 
@@ -8,31 +9,33 @@ def main():
     parser.add_argument("-b", "--backup", default='.bak', help="backup file extension.")
     parser.add_argument("-t", "--threshold", type=int, default=55, help="maximum characters per line.")
     parser.add_argument("-p", "--preview", action="store_true", help="preview result without modifying the file.")
+    parser.add_argument("--encoding", default="utf-8", help="encoding of the files.")
 
     args = parser.parse_args()
     bak = args.backup
     threshold = args.threshold
     preview = args.preview
     files = args.files
+    encoding = args.encoding
 
     for f in files:
         print('-'*10)
         print(f)
         print('-'*10)
-        wrap_captions(f, bak, threshold, preview)
-    
+        wrap_captions(f, bak, threshold, preview, encoding)
 
-def wrap_captions(filename, bak, threshold, preview):
+
+def wrap_captions(filename, bak, threshold, preview, encoding):
     if preview:
-        with open(filename, 'r') as f:
+        with io.open(filename, 'r', encoding=encoding) as f:
             for line in f:
                 output = process_line(line, threshold)
                 print(output)
     else:
-        with in_place.InPlace(filename, backup_ext=bak) as f:        
+        with in_place.InPlace(filename, backup_ext=bak, encoding=encoding) as f:
             for line in f:
                 output = process_line(line, threshold)
-                f.write("{}\n".format(output))
+                f.write(u"{}\n".format(output))
 
 
 def process_line(line, threshold):
@@ -49,7 +52,7 @@ def process_line(line, threshold):
             else:
                 back.append(item)
             count += 1
-        output = "{}\n{}".format(" ".join(front), " ".join(back))
+        output = u"{}\n{}".format(" ".join(front), " ".join(back))
     else:
         output = line
     return output
